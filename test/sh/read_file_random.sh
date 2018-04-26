3d93fd6a7a3179e58773eb847a7d05e0e26dd688
Jim Huang <jimhuaang@outlook.com>
2018-04-26 13:19:37 +0800

    Add function test ReadFileRandom

---
+++ test/sh/read_file_random.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# +-------------------------------------------------------------------------
+# | Copyright (C) 2018 Yunify, Inc.
+# +-------------------------------------------------------------------------
+# | Licensed under the Apache License, Version 2.0 (the "License");
+# | You may not use this work except in compliance with the License.
+# | You may obtain a copy of the License in the LICENSE file, or at:
+# |
+# | http://www.apache.org/licenses/LICENSE-2.0
+# |
+# | Unless required by applicable law or agreed to in writing, software
+# | distributed under the License is distributed on an "AS IS" BASIS,
+# | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# | See the License for the specific language governing permissions and
+# | limitations under the License.
+# +-------------------------------------------------------------------------
+#
+#
+# test case: random read file
+
+set -o xtrace
+set -o errexit
+
+current_path=$(dirname "$0")
+source "$current_path/utils.sh"
+
+FILE_NAME='random_read_file.txt'
+MAX_NUM=30
+HEAD_COUNT=8
+
+# creat a file at first
+append_test_file $FILE_NAME $MAX_NUM
+
+# random read
+random_numbers=( $(shuf -n $HEAD_COUNT "$QSFS_TEST_RUN_DIR/$FILE_NAME") )
+
+# validation
+count_=${#random_numbers[@]}
+if [ $count_ -ne $HEAD_COUNT ]; then
+  echo "Error: expected random read count ${HEAD_COUNT}, got ${count_}"
+  exit 1
+fi
+for i in ${random_numbers[@]}; do
+  if [ $i -lt 1 ] || [ $i -gt $MAX_NUM ]; then
+    echo "Error: expected number belong to [1,$HEAD_COUNT], got $i"
+    exit 1
+  fi
+done
+
+# clean up
+rm_test_file $FILE_NAME
